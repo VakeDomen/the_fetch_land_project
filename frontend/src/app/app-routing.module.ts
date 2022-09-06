892c21f2823717f00aff4dc700f1e0a0e08a4e27
VakeDomen <vake.domen94@gmail.com>
2022-09-06 11:41:35 +0200

    FE structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FrontComponent } from './pages/front/front.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SaleComponent } from './pages/sale/sale.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "contact",
+    component: ContactComponent,
+  },
+  {
+    path: "profile",
+    component: ProfileComponent,
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
+  },
+  {
+    path: "sale/{id}",
+    component: SaleComponent,
+  },
+  {
+    path: "**",
+    component: FrontComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontComponent } from './pages/front/front.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SaleComponent } from './pages/sale/sale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontComponent,
+    ContactComponent,
+    ProfileComponent,
+    RegisterComponent,
+    SaleComponent
   ],
   imports: [
     BrowserModule,
