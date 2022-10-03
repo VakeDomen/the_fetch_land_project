2c7b63acdcbbf7343567518f2e62f89df3f60454
VakeDomen <vake.domen94@gmail.com>
2022-10-03 16:02:21 +0200

    added move to build script

--- build_fe.sh
+++ build_fe.sh
@@ -2,3 +2,4 @@ cd frontend
 sudo rm -r node_modules
 npx -p @angular/cli@14 npm install
 npx -p @angular/cli@14 ng build --configuration production
+sudo mv /home/vake/the_fetch_land_project/frontend/dist/the-fetch-land-project /var/www/fetchland.eu
