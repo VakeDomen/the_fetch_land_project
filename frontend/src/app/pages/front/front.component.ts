93edefecafd0031c9c7155e4a7077d3840471463
maki1337 <matevz.mak@gmail.com>
2022-10-05 20:54:54 +0200

    ui changes

--- frontend/src/app/pages/front/front.component.ts
+++ frontend/src/app/pages/front/front.component.ts
@@ -5,23 +5,19 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-front',
   templateUrl: './front.component.html',
-  styleUrls: ['./front.component.sass']
+  styleUrls: ['./front.component.scss'],
 })
 export class FrontComponent implements OnInit {
-
   public searchQuery: string = '';
 
-  constructor(
-    private router: Router,
-    private title: Title,
-  ) { }
+  constructor(private router: Router, private title: Title) {}
 
   ngOnInit(): void {
-    this.title.setTitle("TheFethclandProject");
+    this.title.setTitle('TheFethclandProject');
   }
 
   public search() {
-    sessionStorage.setItem("saleSearchQeury", this.searchQuery);
-    this.router.navigate(["search"]);
+    sessionStorage.setItem('saleSearchQeury', this.searchQuery);
+    this.router.navigate(['search']);
   }
 }
