b1aeb3a56fbb528745dc93eb2d5cbfbab2b20836
VakeDomen <vake.domen94@gmail.com>
2022-10-11 20:48:49 +0200

    ssr build scripts

--- build_fe.sh
+++ build_fe.sh
@@ -1,7 +1,12 @@
 cd frontend
 sudo rm -r node_modules
 npx -p @angular/cli@14 npm install
-npx -p @angular/cli@14 ng build --configuration production
-sudo mv /var/www/fetchland.eu /var/www/fetchland.eu.old
-sudo mv /home/vake/the_fetch_land_project/frontend/dist/the-fetch-land-project /var/www/fetchland.eu
-sudo rm -r /var/www/fetchland.eu.old
+# -- SSR
+npx -p @angular/cli@14 npm run build:ssr
+systemctl restart fetchland_web.service
+
+# -- non SSR
+#npx -p @angular/cli@14 ng build --configuration production
+#sudo mv /var/www/fetchland.eu /var/www/fetchland.eu.old
+#sudo mv /home/vake/the_fetch_land_project/frontend/dist/the-fetch-land-project /var/www/fetchland.eu
+#sudo rm -r /var/www/fetchland.eu.old
