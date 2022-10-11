1a74e1a5dfe02cec882345c92e9a7e570ef39a80
VakeDomen <vake.domen94@gmail.com>
2022-10-11 12:40:28 +0200

    added FE mv to build script

--- build_fe.sh
+++ build_fe.sh
@@ -2,4 +2,6 @@ cd frontend
 sudo rm -r node_modules
 npx -p @angular/cli@14 npm install
 npx -p @angular/cli@14 ng build --configuration production
+sudo mv /var/www/fetchland.eu /var/www/fetchland.eu.old
 sudo mv /home/vake/the_fetch_land_project/frontend/dist/the-fetch-land-project /var/www/fetchland.eu
+sudo rm -r /var/www/fetchland.eu.old
